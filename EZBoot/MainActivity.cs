550aaf5332849a1c44916d032aa95b488be18d16
TKDabrowski <tdabrowski2206@gmail.com>
2018-12-04 17:21:44 +0000

    Initial commit

---
+++ EZBoot/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace EZBoot
+{
+    [Activity(Label = "EZBoot Login Screen", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //Initialize elements to make them functional
+            Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            Button signUpButton = FindViewById<Button>(Resource.Id.signUpButton);
+            EditText userName = FindViewById<EditText>(Resource.Id.userName);
+            EditText userPassword = FindViewById<EditText>(Resource.Id.userPassword);
+
+
+            //Button click function for login button
+            loginButton.Click += (o, e) => {
+                Validator validator = new Validator(userName.Text, userPassword.Text);
+                bool validation;
+                validation = validator.validateUser();
+
+                if (validation == true)
+                {
+                    Toast.MakeText(this, "You have logged in", ToastLength.Long).Show();
+                    Intent transferPages = new Intent(this, typeof(MainScreen));
+                    this.StartActivity(transferPages);
+                }
+                else
+                {
+                    Toast.MakeText(this, "Login attempt failed, try again", ToastLength.Long).Show();
+                }   
+            };
+
+            //Button click function for sign up button
+            signUpButton.Click += (o, e) =>
+            {
+                Intent transferPages = new Intent(this, typeof(SignUpScreen));
+            };
+        }
+    }
+}
---
+++ EZBoot/SignUpScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EZBoot
+{
+    [Activity(Label = "EZBoot Sign Up")]
+    public class SignUpScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SignUp);
+
+            // Create your application here
+        }
+    }
+}
